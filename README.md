420570a5ba903daad8b12c17846c89671c921edc
Haoshka <snakemanzh@gmail.com>
2025-11-06 15:59:32 +0000

    add monitering

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # llm_zoomcamp
 learning
+
+
+## useful shell command
+docker run -it --rm \
+    --network="llm_zoomcamp_default" \
+    --env-file=".env" \
+    -e GROQ_API_KEY=${GROQ_API_KEY} \
+    -e DATA_PATH="data/data.csv" \
+    -p 5000:5000 \
+    app
+
+
+
+pipenv shell
+
+cd app/
+
+export POSTGRES_HOST=localhost
+python app.py
+
+
+
+docker run -it --rm app bash
+ls -R /app
+python - <<'EOF'
+from pathlib import Path
+print(list(Path("/app/data").glob("*")))
+EOF
--- app/ingest.py
+++ app/ingest.py
@@ -1,29 +1,29 @@
+import os
 import pandas as pd
+
 import minsearch
 
 
-def load_index(data_path='../data/data.csv'):
-    # Load data from CSV and create search index
-    
-    df = pd.read_csv(data_path)
-    # df.columns = df.columns.str.lower()
+DATA_PATH = os.getenv("DATA_PATH", "../data/data.csv")
 
-    documents = df.to_dict(orient='records')
+
+def load_index(data_path=DATA_PATH):
+    df = pd.read_csv(data_path)
+
+    documents = df.to_dict(orient="records")
 
     index = minsearch.Index(
         text_fields=[
-            'exercise_name', 
-            'type_of_activity', 
-            'type_of_equipment', 
-            'body_part',
-            'type', 
-            'muscle_groups_activated', 
-            'instructions',
+            "exercise_name",
+            "type_of_activity",
+            "type_of_equipment",
+            "body_part",
+            "type",
+            "muscle_groups_activated",
+            "instructions",
         ],
-        keyword_fields=['id']
+        keyword_fields=["id"],
     )
 
     index.fit(documents)
-
     return index
-
